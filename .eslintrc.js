967ad5e2bb05ebe8ba0a22398b2f9b18f5133df4
mmarkelov <maks-markel@mail.ru>
2020-02-02 15:18:39 +0300

    Add eslint, prettier, husky and lint-staged

---
+++ .eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+  root: true,
+  extends: ['airbnb-base', 'prettier'],
+  parser: 'babel-eslint',
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  env: {
+    node: true,
+    jest: true,
+    browser: true,
+  },
+  globals: {
+    page: true,
+    browser: true,
+    context: true,
+  },
+}
