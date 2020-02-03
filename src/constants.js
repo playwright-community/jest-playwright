1c5dec1992df9a96503cd92313c08cbb8dbcc1b4
mmarkelov <maks-markel@mail.ru>
2020-02-03 14:17:20 +0300

    Fix prettier errors

--- src/constants.js
+++ src/constants.js
@@ -1,16 +1,16 @@
 import path from 'path'
 import os from 'os'
 
-export const DIR = path.join(os.tmpdir(), 'jest_playwright_global_setup');
-export const WS_ENDPOINT_PATH = path.join(DIR, 'wsEndpoint');
+export const DIR = path.join(os.tmpdir(), 'jest_playwright_global_setup')
+export const WS_ENDPOINT_PATH = path.join(DIR, 'wsEndpoint')
 
-export const CHROMIUM = 'chromium';
-export const FIREFOX = 'firefox';
-export const WEBKIT = 'webkit';
+export const CHROMIUM = 'chromium'
+export const FIREFOX = 'firefox'
+export const WEBKIT = 'webkit'
 
 export const DEFAULT_CONFIG = {
-	launchBrowserApp: { webSocket: true},
-	context: {},
-	browser: CHROMIUM,
-	exitOnPageError: true,
-};
+  launchBrowserApp: { webSocket: true },
+  context: {},
+  browser: CHROMIUM,
+  exitOnPageError: true,
+}
