16c34d96025de945b3a34a33bb219cba6fb8e20e
Max Schmitt <max@schmitt.mx>
2020-02-23 10:04:04 +0100

    fix: it

--- types/global.d.ts
+++ types/global.d.ts
@@ -1,5 +1,23 @@
-import { Page } from 'playwright-core';
+import { Page, Browser, BrowserContext } from 'playwright-core';
+
+interface JestPlaywright {
+  /**
+   * Suspends test execution and gives you opportunity to see what's going on in the browser
+   * - Jest is suspended (no timeout)
+   * - A debugger instruction to the Browser, if Playwright has been launched with { devtools: true } it will stop
+   *
+   * ```ts
+   * it('should put test in debug mode', async () => {
+   *   await jestPlaywright.debug()
+   * })
+   * ```
+   */
+  debug: () => Promise<void>
+}
 
 declare global {
-  const page: Page;
+  var page: Page;
+  var browser: Browser
+  var context: BrowserContext
+  var jestPlaywright: JestPlaywright
 }
