88fbc83ea72cd7563dacd378a961b11ef5870db2
Max Schmitt <max@schmitt.mx>
2020-02-23 10:24:07 +0100

    fix: string -> BrowserType

--- src/bin/testProcess.ts
+++ src/bin/testProcess.ts
@@ -1,9 +1,9 @@
 import { spawn, spawnSync, SpawnSyncOptions } from 'child_process'
 import { readConfig } from '../utils'
 import { checkBrowsers } from './utils'
-import { PARALLEL } from '../constants'
+import { PARALLEL, BrowserType } from '../constants'
 
-const getSpawnOptions = (browser: string): SpawnSyncOptions => ({
+const getSpawnOptions = (browser: BrowserType): SpawnSyncOptions => ({
   stdio: 'inherit',
   shell: true,
   env: {
@@ -14,7 +14,7 @@ const getSpawnOptions = (browser: string): SpawnSyncOptions => ({
 
 const exec = ({ sequence, browser, params }: {
   sequence: string,
-  browser: string,
+  browser: BrowserType,
   params: string[]
 }) => {
   // TODO Add messages for browser process
--- src/bin/utils.ts
+++ src/bin/utils.ts
@@ -1,7 +1,8 @@
 import { checkBrowserEnv } from '../utils'
+import { BrowserType } from '../constants'
 
 /* eslint import/prefer-default-export:0 */
-export function checkBrowsers(browsers?: string[]) {
+export function checkBrowsers(browsers?: BrowserType[]) {
   if (!browsers || !browsers.length) {
     throw new Error(
       'You should define browsers with your jest-playwright.config.js',
