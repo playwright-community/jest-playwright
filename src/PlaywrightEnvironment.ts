cf3e5908674a6696705eda9dc8feeb72687e3a79
Maksim Markelov <maks-markel@mail.ru>
2021-01-05 20:16:25 +0300

    Fix axios vulnerability (#523)
    * Small ts improvements
    
    * Update dependencies

--- src/PlaywrightEnvironment.ts
+++ src/PlaywrightEnvironment.ts
@@ -51,7 +51,7 @@ const getBrowserPerProcess = async (
 
   if (launchType === LAUNCH || launchType === PERSISTENT) {
     // https://github.com/mmarkelov/jest-playwright/issues/42#issuecomment-589170220
-    if (browserType !== CHROMIUM && launchOptions && launchOptions.args) {
+    if (browserType !== CHROMIUM && launchOptions?.args) {
       launchOptions.args = launchOptions.args.filter(
         (item: string) => item !== '--no-sandbox',
       )
