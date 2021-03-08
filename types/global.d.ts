13acc493ca79bd74605e0e35aeaf8ba3bc07442b
Maksim Markelov <maks-markel@mail.ru>
2021-03-08 14:07:03 +0300

    Remove debug from types

--- types/global.d.ts
+++ types/global.d.ts
@@ -77,18 +77,6 @@ interface JestPlaywright {
    */
   resetBrowser: (newOptions?: BrowserContextOptions) => Promise<void>
   /**
-   * Suspends test execution and gives you opportunity to see what's going on in the browser
-   * - Jest is suspended (no timeout)
-   * - A debugger instruction to the Browser, if Playwright has been launched with { devtools: true } it will stop
-   *
-   * ```ts
-   * it('should put test in debug mode', async () => {
-   *   await jestPlaywright.debug()
-   * })
-   * ```
-   */
-  debug: () => Promise<void>
-  /**
    * Saves the coverage to the disk which will only work if `collectCoverage`
    * in `jest-playwright.config.js` file is set to true. The merged coverage file
    * is then available in `.nyc_output/coverage.json`. Mostly its needed in the
